493768930e52c147583046f469082e7b404aeb37
Grigorij Kaplan <grigorij@grigorijs-mbp.lt.ibm.com>
2019-02-08 11:47:05 +0200

    prefixed added to the deployment procedure

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 set -x #echo on
 
+ACTION_PREFIX=NNN-
+CL_PACKAGE=lab-feedback-db-package/
+SEQ_READ=${ACTION_PREFIX}save-feedback-entry-sequence
+SEQ_WRITE=${ACTION_PREFIX}read-feedback-entry-sequence
+
+# logging into IBM Cloud
 REGION_CODE="${PROD_REGION_ID//ibm:yp:/}"
 bx login --apikey ${DEPLOYER_API_KEY} -a https://api.${REGION_CODE}.bluemix.net -o ${PROD_ORG_NAME} -s ${PROD_SPACE_NAME}
 
+# creating cloudant package
+bx fn package bind /whisk.system/cloudant lab-feedback-db-package -p dbname feedback
+
+# binding credentials
+bx fn service bind cloudantNoSQLDB lab-feedback-db-package --instance feedback-db-alias --keyname serverless-function-credentials
 
 # creating the first action
-./pipeline/create-action.sh prepare-entry-for-save prepare-entry-for-save.js 
+./pipeline/create-action.sh ${ACTION_PREFIX}prepare-entry-for-save prepare-entry-for-save.js 
 
 # creating the second action
-./pipeline/create-action.sh set-read-input set-read-input.js 
+./pipeline/create-action.sh ${ACTION_PREFIX}set-read-input set-read-input.js 
 
 # creating the third action
-./pipeline/create-action.sh format-entries format-entries.js 
+./pipeline/create-action.sh ${ACTION_PREFIX}format-entries format-entries.js 
 
 # creating sequences
 
-./pipeline/create-action.sh save-feedback-entry-sequence prepare-entry-for-save,binding-for-feedback/create-document --sequence --web true
+./pipeline/create-action.sh ${SEQ_READ} ${ACTION_PREFIX}prepare-entry-for-save,${CL_PACKAGE}create-document --sequence --web true
 
-./pipeline/create-action.sh read-feedback-entry-sequence set-read-input,binding-for-feedback/list-documents,format-entries --sequence --web true
+./pipeline/create-action.sh ${SEQ_WRITE} ${ACTION_PREFIX}set-read-input,${CL_PACKAGE}list-documents,${ACTION_PREFIX}format-entries --sequence --web true
 
 # create APIs
 
-bx fn api create /feedback /entries GET read-feedback-entry-sequence
+bx fn api create /${ACTION_PREFIX}feedback /entries GET ${SEQ_READ}
 
-bx fn api create /feedback /entries POST save-feedback-entry-sequence
+bx fn api create /${ACTION_PREFIX}feedback /entries POST ${SEQ_WRITE}
